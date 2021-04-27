5a717e651d6d2c7840890f1f39b4f72b0aa6fc77
rkbrian <rkbriankong@gmail.com>
2021-04-27 19:18:24 +0000

    3 and 4 tests

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,1 +1,4 @@
 #!/usr/bin/python3
+for istopher in range(97, 123):
+    if istopher != 101 and istopher != 113:
+        print("{}".format(chr(istopher)), end="")
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for agon in range(99):
+    print("{0} = {1}".format(agon, hex(agon)))
