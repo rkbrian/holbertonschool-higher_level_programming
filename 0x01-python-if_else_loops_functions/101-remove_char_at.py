676294df181109daf914e7bfdd2436235c7589aa
rkbrian <rkbriankong@gmail.com>
2021-04-28 00:15:56 +0000

    15 checking negative n

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    newstr = str[:n] + str[n+1:]
-    return (newstr)
+    if n >= 0:
+        newstr = str[:n] + str[n+1:]
+        return (newstr)
+    else:
+        return (str)
