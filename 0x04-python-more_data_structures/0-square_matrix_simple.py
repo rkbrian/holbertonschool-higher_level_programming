18d565f0391b12fb9438e48b03cc170f5ec4a8c8
rkbrian <rkbriankong@gmail.com>
2021-04-29 22:32:58 +0000

    0 rev 5: typo fixed

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    Keanu = [[]]
-    for i in range(len(matrix)):
-        Keanu[i] = list(map(lambda j : j ** 2, matrix[i]))
+    Keanu = [list(map(lambda j : j ** 2, theone)) for theone in matrix]
     return Keanu
