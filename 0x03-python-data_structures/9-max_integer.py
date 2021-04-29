742d81d9193b09d007de188aaebf3f6d366b6152
rkbrian <rkbriankong@gmail.com>
2021-04-29 03:42:35 +0000

    9 quick fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maxwell = my_list[0]
-    for i in range(len(my_list) - 1):
-        if maxwell < my_list[i]:
-            maxwell = my_list[i]
-    return maxwell
+    if not my_list:
+        return None
+    else:
+        maxwell = my_list[0]
+        for i in range(len(my_list) - 1):
+            if maxwell < my_list[i]:
+                maxwell = my_list[i]
+        return maxwell
