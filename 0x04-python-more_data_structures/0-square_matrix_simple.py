2313d58d20d826ed9226b7554a51ec89f070ccbf
rkbrian <rkbriankong@gmail.com>
2021-04-29 22:35:31 +0000

    0 rev 6

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    Keanu = [list(map(lambda j : j ** 2, theone)) for theone in matrix]
-    return Keanu
+    return [list(map(lambda j : j ** 2, theone)) for theone in matrix]
