7303e280bbf0857e85667bc7e8b28bccb395f6b5
rkbrian <rkbriankong@gmail.com>
2021-04-30 03:48:12 +0000

    12 test 1

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    rody = ['I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000]
-    for latinum in roman_string:
-        
+    rody = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    if roman_string == None or type(roman_string) != 'str':
+        return 0
+    else:
+        for latinum in roman_string:
+            num += rody.get(latinum)
+        lasti = len(roman_string) - 1
+        if roman_string[lasti - 1] == 'I' and roman_string[lasti] != 'I':
+            num -= 2
+        return num
