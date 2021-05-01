b39266767e6de1ca691a57dd315cedcdf4a44158
rkbrian <rkbriankong@gmail.com>
2021-05-01 03:50:08 +0000

    4 fixed code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from hidden_4 import name
+import hidden_4
 if __name__ == "__main__":
-    for i in range(len(name)):
-        if name[i][0] != chr(95):
-            print("{}".format(name[i]))
+    for i in range(len(dir(hidden_4))):
+        thename = dir(hidden_4)[i]
+        if thename[0] == "_" and thename[1] == "_":
+            continue
+        print("{}".format(thename))
