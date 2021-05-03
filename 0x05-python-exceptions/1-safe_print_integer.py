ee54bb760d06c5b6e4205e88949ead77ccd5b0c1
rkbrian <rkbriankong@gmail.com>
2021-05-03 21:17:36 +0000

    1 test

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def safe_print_integer(value):
+    if not value:
+        return 0
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,18 @@
-/usr/bin/python3
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    if not my_list:
+        return 0
+    counti = 0
+    printi = 0
+    for 
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            printi += 1
+            counti += 1
+            if counti == x:
+                print()
+        except:
+            print()
+            return printi
+    return printi
