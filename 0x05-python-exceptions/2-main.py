ac6d14ab61b2a2dc1712843a09ffb04c62aeb3fc
rkbrian <rkbriankong@gmail.com>
2021-05-03 22:32:23 +0000

    2 test

--- 0x05-python-exceptions/2-main.py
+++ 0x05-python-exceptions/2-main.py
@@ -1,4 +1,4 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 safe_print_list_integers = \
     __import__('2-safe_print_list_integers').safe_print_list_integers
 
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,15 +4,13 @@ def safe_print_list_integers(my_list=[], x=0):
         return 0
     counti = 0
     printi = 0
-    for 
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
-            printi += 1
-            counti += 1
-            if counti == x:
-                print()
-        except:
-            print()
-            return printi
+            if isinstance(i, int):
+                print("{:d}".format(my_list[i]), end="")
+                printi += 1
+                counti += 1
+        except (TypeError, ValueError):
+            print("", end="")
+    print()
     return printi
