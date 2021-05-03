5effceba751f4ed49b652630010bb4afc966e342
rkbrian <rkbriankong@gmail.com>
2021-05-03 22:56:33 +0000

    3 test

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,8 @@
-/usr/bin/python3
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        c = a / b
+    except:
+        c = None
+    print("Inside result: {}".format(c))
+    return c
