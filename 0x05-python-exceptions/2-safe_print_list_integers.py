8c996d00cf5c815477c7fee8263d51b88f36fff8
rkbrian <rkbriankong@gmail.com>
2021-05-04 05:41:30 +0000

    2 rev 1 delete edge case

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    if not my_list:
-        return 0
     counti = 0
-    printi = 0
     for i in range(x):
         try:
             if isinstance(i, int):
                 print("{:d}".format(my_list[i]), end="")
-                printi += 1
                 counti += 1
         except (TypeError, ValueError):
             print("", end="")
     print()
-    return printi
+    return counti
