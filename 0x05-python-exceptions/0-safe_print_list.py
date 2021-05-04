31155fde29cd433f691e55eda24e5d8f16ee7a01
rkbrian <rkbriankong@gmail.com>
2021-05-04 05:21:37 +0000

    0 rev 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
             counti += 1
             if counti == x:
                 print()
-        except:
+        except IndexError:
             print()
             return printi
     return printi
