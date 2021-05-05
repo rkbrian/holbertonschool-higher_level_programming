702c9f7a3c42262a1dbc307895a874621d43b91a
rkbrian <rkbriankong@gmail.com>
2021-05-05 04:32:16 +0000

    8 test: __str__ method, return captured print instance, fix new line problem

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+""" class Square: contains the following items:
+    size: private instance attribute
+    TypeError: type error messages
+    ValueError: value error messages
+    area: public instance method
+"""
+
+
+class Square:
+    """ def and initiate size, manage output, no type preference, raise err """
+
+    @property
+    def size(self):
+        """getter to retrieve size"""
+        return self.__size
+
+    @property
+    def position(self):
+        """getter to retrieve position"""
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        """setter to set size value"""
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @position.setter
+    def position(self, value):
+        """setter to set position"""
+        self.__position = value
+
+    def __init__(self, size=0, position=(0, 0)):
+        """initialize objects"""
+        self.__size = size
+        if isinstance(position, tuple) is False:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(position) < 2 or isinstance(position[1], int) is False:
+            raise IndexError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
+            raise ValueError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    def __str__(self, size=0, position=(0, 0)):
+        """customized __str__ method to smart return instance attributes"""
+        return str(self.my_print())
+
+    def area(self):
+        """area calculation"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """hashtag matrix printing"""
+        if self.__size == 0:
+            print()
+        elif self.__position is None:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for j in range(self.__size - 1):
+                print("{}".format(" ") * self.__position[0], end="")
+                print("{}".format("#") * self.__size, end="")
+                print()
+            print("{}".format(" ") * self.__position[0], end="")
+            print("{}".format("#") * self.__size, end="")
+        return ''
