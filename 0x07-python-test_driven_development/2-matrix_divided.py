518271a299fa5b6dc2ad2415ea547286735b002f
rkbrian <rkbriankong@gmail.com>
2021-05-05 23:32:56 +0000

    2 fixed

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,15 +11,17 @@ def matrix_divided(matrix, div):
         return the divided matrix
     """
 
-    try:
-        Keanu = [list(map(lambda j: round(j / div, 2), Neo)) for Neo in matrix]
-        return Keanu
-    except:
-        if type(k) != int and type(k) != float for k in matrix:
-            raise TypeError("Each row of the matrix must have the same size")
-        if len(matrix[i]) != len(matrix[0]) for i in range(len(matrix)):
-            raise TypeError("Each row of the matrix must have the same size")
-        if div == 0:
-            raise ZeroDivisionError("division by zero")
-        if type(div) != int and type(div) != float
-            raise TypeError("div must be a number")
+    for k in range(len(matrix)):
+        for l in range(len(matrix[k])):
+            if type(matrix[k][l]) != int and type(matrix[k][l]) != float:
+                raise TypeError
+                ("matrix must be a matrix (list of lists) of integers/floats")
+        if len(matrix[k]) != len(matrix[0]):
+            raise TypeError
+            ("Each row of the matrix must have the same size")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+    Keanu = [list(map(lambda j: round(j / div, 2), Neo)) for Neo in matrix]
+    return Keanu
