aeaf0939a1667460689f9af5471bc927300ef56e
rkbrian <rkbriankong@gmail.com>
2021-05-10 20:59:39 +0000

    module imput doesn't work the same way as direct statement, unless it's sys call

--- 0x09-python-everything_is_object/t.py
+++ 0x09-python-everything_is_object/t.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-a = [1, 2, 3, 4]
-print(id(a))
+a = [1, 2, 3]
+print("ID of a is {}".format(id(a)))
+b = a
+print("ID of b is {}".format(id(b)))
+print("a is b? {}".format(a is b))
+
+a += [4]
+print("after a += [4]")
 print(a)
+print(b)
+print("ID of a is {}".format(id(a)))
+print("ID of b is {}".format(id(b)))
