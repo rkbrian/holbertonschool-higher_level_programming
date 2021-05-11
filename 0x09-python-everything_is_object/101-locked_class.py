08688d1523054b94b48f53769a424480e491c420
rkbrian <rkbriankong@gmail.com>
2021-05-11 06:33:08 +0000

    101 test

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Locked_Class
+"""
+
+
+class Locked_Class():
+    """Class to prevents the user from dynamically creating
+    new instance attributes.
+    """
+    __slots__ = ["first_name"]
--- 0x09-python-everything_is_object/t.py
+++ 0x09-python-everything_is_object/t.py
@@ -11,3 +11,11 @@ print(a)
 print(b)
 print("ID of a is {}".format(id(a)))
 print("ID of b is {}".format(id(b)))
+
+c = 5
+print("ID of c is {}".format(id(c)))
+d = c
+print("ID of d is {}".format(id(d)))
+print("c is d? {}".format(c is d))
+c += 1
+print("ID of c is {}".format(id(c)))
