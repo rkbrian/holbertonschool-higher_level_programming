4c8e1de4490aa809e75a0425445461f4130c684f
rkbrian <rkbriankong@gmail.com>
2021-05-12 02:20:54 +0000

    11 test

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,9 +12,9 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Instantiation with size"""
+        super().integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
-        super().integer_validator("size", size)
 
     def area(self):
         """the area method implementation"""
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+module for Square, the class with some functions in it
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.__size = size
+        super().__init__(size, size)
+        super().integer_validator("size", size)
+
+    def area(self):
+        """the area method implementation"""
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """return the Square info to print"""
+        return ("[Square]{}/{}".format(self.__size, self.__size))
