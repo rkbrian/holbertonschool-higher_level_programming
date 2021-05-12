9d1dc042cfecea24e8349d9cd791912fbf89723a
rkbrian <rkbriankong@gmail.com>
2021-05-12 01:48:11 +0000

    10 test

--- 0x0A-python-inheritance/9-main.py
+++ 0x0A-python-inheritance/9-main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
 
-r = Rectangle(3, 5)
-
+r = Rectangle(1411, 781)
 print(r)
-print(r.area())
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -23,4 +23,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """return to the Rectangle info to print"""
-        return ("[Rectange] {}/{}".format(self.__width, self.__height))
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
