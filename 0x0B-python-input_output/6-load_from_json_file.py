3322150758341a44836e286d801c6a07abeeaa64
rkbrian <rkbriankong@gmail.com>
2021-05-12 22:15:37 +0000

    6 test 2

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -2,10 +2,10 @@
 """module for function load_from_json_file"""
 
 
-import json
 def load_from_json_file(filename):
     """function to create an object from the given "JSON file"."""
 
+    import json
     with open(filename) as BourneUltimatum:
         Jason = json.load(BourneUltimatum)
         return Jason
