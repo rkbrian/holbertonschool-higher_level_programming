2f36a2d5edda0f9d621a7f67c0ae480500b3f5a7
rkbrian <rkbriankong@gmail.com>
2021-05-12 02:49:46 +0000

    11 msg control for error size

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -13,7 +13,7 @@ class Square(Rectangle):
     def __init__(self, size):
         """Instantiation with size"""
         self.__size = size
-        super().__init__(size, size)
+        Rectangle.__init__(self, size, size)
         super().integer_validator("size", size)
 
     def area(self):
