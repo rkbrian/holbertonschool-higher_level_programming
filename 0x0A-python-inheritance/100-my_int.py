f2b128fac5f3ec51092465a1d4488b2315d6403d
rkbrian <rkbriankong@gmail.com>
2021-05-12 04:59:57 +0000

    100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+module for class MyInt
+"""
+
+
+class MyInt(int):
+    """functions to show the opposite Boolean value"""
+
+    def __init__(self, number):
+        """initializing the input number"""
+        self.number = number
+
+    def __eq__(self, exnum):
+        """excluded number"""
+        return self.number == exnum.number
+
+    def __ne__(self, exnum):
+        """3 as the opposed target"""
+            return not self.__eq__(exnum)
