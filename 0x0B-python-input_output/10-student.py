287f54a13869d6d45fb0e66652aab61645aedc54
rkbrian <rkbriankong@gmail.com>
2021-05-13 01:33:26 +0000

    10 test

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
-"""
+"""module for class Student, the class of info of students"""
+
+
+class Student:
+    """including first name, last name, age"""
+
+    def __init__(self, first_name, last_name, age):
+        """instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieving dictionary representation"""
+        dictcopy = self.__dict__.copy()
+        xliteral = {}
+        if type(attrs) == list:
+            for j in dictcopy:
+                for i in range(len(attrs)):
+                    if j == attrs[i]:
+                        xliteral[j] = dictcopy[j]
+            return xliteral
+        return self.__dict__
