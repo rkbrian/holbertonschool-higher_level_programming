26b942a23998a13f62a2e26f68c568cffb8a007f
rkbrian <rkbriankong@gmail.com>
2021-05-13 03:16:14 +0000

    12 test

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+module for function pascal_triangle
+It looks so fun! Hahahahaha...
+Hahahahahahahahahahahahahahahahahaha......
+"""
+
+
+def pascal_triangle(n):
+    """function to create pasccal triangle"""
+
+    list_a = [1]
+    list_b = [1]
+    list_c = []
+    lily = []
+    print(list_a)
+    for i in range(1, n):
+        list_a.insert(0, 0)
+        list_b.append(0)
+        list_c = [a + b for a, b in zip(list_a, list_b)]
+        lily.append(list_c)
+        list_a = list(list_c)
+        list_b = list(list_c)
+    return(lily)
