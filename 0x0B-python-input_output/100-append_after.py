79121e492e263a022bc3b5d90c6d20124b1f8da3
rkbrian <rkbriankong@gmail.com>
2021-05-13 05:14:36 +0000

    100 rev 1

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,7 +6,7 @@ def append_after(filename="", search_string="", new_string=""):
     """function to insert a text line to a file"""
 
     writeall = ""
-    with open(filename, mode="r", encoding="utf-8") as Cut_in_line:
+    with open(filename, mode="r") as Cut_in_line:
         readall = Cut_in_line.readline()
         while readall:
             if search_string in readall:
@@ -14,5 +14,5 @@ def append_after(filename="", search_string="", new_string=""):
             else:
                 writeall += readall
             readall = Cut_in_line.readline()
-    with open(filename, mode="w", encoding="utf-8") as Cut_in_line:
+    with open(filename, mode="w") as Cut_in_line:
         Cut_in_line.write(writeall)
