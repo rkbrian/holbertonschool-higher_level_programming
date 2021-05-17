63c30f4565f395151a3b47f93fc7d64b6bbae312
rkbrian <rkbriankong@gmail.com>
2021-05-17 21:02:41 +0000

    18 rev 1 and 19 test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,7 +21,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """static method to return JSON str rep"""
-        import json
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
@@ -46,15 +45,38 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """class method to return an instance with all attr set"""
-        """note to myself:
+        """Note to myself:
         since the 1st obj (id) is always 1, I need to
         differenciate Rectangle and Square by changing the 2nd obj
         """
         if cls.__name__ == "Rectangle":
             dummy = cls(1, 1)
         elif cls.__name__ == "Square":
-            dummy = cls(1, 2)
+            dummy = cls(1)
         else:
             dummy = cls()
         dummy.update(**dictionary)
         return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """return a list of instances from content of json file"""
+        """Note to myself: the procedures
+        capture json string from json file and convert it;
+        making dictionaries for all the instances in the converted;
+        distribute instance from dictionaries to the class for update
+        """
+        from os import path
+        fname = cls.__name__ + ".json"
+        if path.exists(fname) is False:
+            return []
+        else:
+            with open(fname, mode="r") as JasonBorn:
+                json_str = JasonBorn.read()
+                reg_str = cls.from_json_string(json_str)
+            if cls:
+                linst = []
+                for x in reg_str:
+                    new_dict = dict(x)
+                    linst.append(cls.create(**new_dict))
+            return linst
