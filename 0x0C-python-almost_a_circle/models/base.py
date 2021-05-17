7f8d1ecc86ae0e3aad14770f1f6dddff90a54787
rkbrian <rkbriankong@gmail.com>
2021-05-17 18:46:10 +0000

    17 and 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,6 +26,13 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """static method to return str from JSON str"""
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
         """class method to write JSON str rep"""
@@ -35,3 +42,19 @@ class Base:
                 for c in list_objs:
                     Jason.append(cls.to_dictionary(c))
             JasonBourne.write(Base.to_json_string(Jason))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """class method to return an instance with all attr set"""
+        """note to myself:
+        since the 1st obj (id) is always 1, I need to
+        differenciate Rectangle and Square by changing the 2nd obj
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1, 2)
+        else:
+            dummy = cls()
+        dummy.update(**dictionary)
+        return dummy
