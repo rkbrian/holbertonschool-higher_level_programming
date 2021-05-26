39ed9e8896fec6b728a7a5135acddd7c09beab23
rkbrian <rkbriankong@gmail.com>
2021-05-26 22:49:50 +0000

    7 test

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- 7. First add
+-- Write a script that inserts a new row in the table first_table (database hbtn_0c_0)
+-- ALTER TABLE first_table ADD 89 'Holberton School'
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School')
