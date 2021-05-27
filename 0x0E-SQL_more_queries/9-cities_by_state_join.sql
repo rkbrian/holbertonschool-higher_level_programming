b10ff200dc511916db2bd53972d86898d97f0ce3
Ran Kong <rkbriankong@gmail.com>
2021-05-27 18:56:52 -0500

    9 debug typo

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- 9. Cities by States
 -- Write a script that lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = state.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
