d54c385a1fb3e455420e35b23a9a9fec6114f0b2
Ran Kong <rkbriankong@gmail.com>
2021-05-28 00:33:24 -0500

    18 not working

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -3,5 +3,5 @@
 SELECT tv_shows.title FROM tv_genres RIGHT JOIN
     (tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id)
     ON tv_genres.id = tv_show_genres.genre_id AND tv_genres.name = 'Comedy'
-    WHERE tv_genres.name IS NULL AND COUNT(tv_shows.title) = 1
+    WHERE tv_genres.name IS NULL
     ORDER BY tv_shows.title ASC;
