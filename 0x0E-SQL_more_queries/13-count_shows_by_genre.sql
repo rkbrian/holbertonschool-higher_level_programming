9f7d1ea1c32144976d6aede4d6314807117bcd1a
Ran Kong <rkbriankong@gmail.com>
2021-05-27 21:15:20 -0500

    just 13 itself is already complicated

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- 13. Number of shows by genre
 -- Write a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT tv_show_genres.name AS genre
-    
-    FROM tv_shows LEFT JOIN tv_show_genres
-    ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC,
-    tv_show_genres.genre_id ASC;
+SELECT tv_genres.name AS genre
+    tv_shows_genres.genre_id COUNT(*) AS number_of_shows
+    FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.name = tv_show_genres.genre_id
+    ORDER BY number_of_shows DESC, genre ASC;
