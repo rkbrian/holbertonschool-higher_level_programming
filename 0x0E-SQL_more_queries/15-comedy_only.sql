095a858a0866ab605260b6a7ffaeec17525f9bae
Ran Kong <rkbriankong@gmail.com>
2021-05-27 22:51:24 -0500

    15 rev 1

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -3,4 +3,4 @@
 SELECT tv_genres.name FROM tv_genres INNER JOIN
     (tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id)
     ON tv_genres.id = tv_show_genres.genre_id WHERE tv_genres.name = 'Comedy'
-    ORDER BY tv_genres.name ASC;
+    ORDER BY tv_shows.title ASC;
