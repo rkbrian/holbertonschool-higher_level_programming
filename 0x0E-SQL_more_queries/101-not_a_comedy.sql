99679f3605a34fe08c6914cf69a20574c427eb8f
Ran Kong <rkbriankong@gmail.com>
2021-05-27 23:51:01 -0500

    18 test

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,7 @@
+-- 18. No Comedy tonight!
+-- Write a script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+SELECT tv_shows.title FROM tv_genres RIGHT JOIN
+    (tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id)
+    ON tv_genres.id = tv_show_genres.genre_id AND tv_genres.name = 'Comedy'
+    WHERE tv_genres.name IS NONE
+    ORDER BY tv_shows.title ASC;
