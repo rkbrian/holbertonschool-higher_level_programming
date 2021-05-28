d34d7faec57c45aec8a73e4579af68350b6c2ede
Ran Kong <rkbriankong@gmail.com>
2021-05-27 22:50:06 -0500

    15 test

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,6 @@
+-- 15. Only Comedy
+-- Write a script that lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT tv_genres.name FROM tv_genres INNER JOIN
+    (tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id)
+    ON tv_genres.id = tv_show_genres.genre_id WHERE tv_genres.name = 'Comedy'
+    ORDER BY tv_genres.name ASC;
