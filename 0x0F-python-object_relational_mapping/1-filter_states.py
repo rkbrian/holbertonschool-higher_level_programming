6aec84f2878d50e73455cdaa84d1c1e7b592c7a5
rkbrian <rkbriankong@gmail.com>
2021-05-31 23:21:43 +0000

    debug

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,10 +8,10 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
-                        port="3306",
-                        username=sys.argv[1],
-                        password=sys.argv[2],
-                        db=sys.argv[3])
+                         port=3306,
+                         username=sys.argv[1],
+                         password=sys.argv[2],
+                         db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute(SELECT * FROM states)
     states = cursor.fetchall()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,12 +13,14 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
-                        port="3306",
-                        username=sys.argv[1],
-                        password=sys.argv[2],
-                        db=sys.argv[3])
+                         port=3306,
+                         username=sys.argv[1],
+                         password=sys.argv[2],
+                         db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE BINARY name = %s", (sys.argv[4]))
+    cursor.execute(
+        "SELECT * FROM states WHERE BINARY name = %s",
+        (sys.argv[4]))
     states = cursor.fetchall()
     for i in range(len(states)):
         print(states[i])
