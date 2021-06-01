def921ddff7c935fb50b42751602e59c5b8288dc
Ran Kong <rkbriankong@gmail.com>
2021-05-31 19:01:04 -0500

    1 and 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Write a script that lists all states with a name starting with N (upper N) from the database"""
+"""
+    Write a script that lists all states with a name starting
+    with N (upper N) from the database
+"""
 
 
 import sys
