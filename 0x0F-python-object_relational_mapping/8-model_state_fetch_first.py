978bdb334dcdac30de724cc9d661b68ebd78207e
rkbrian <rkbriankong@gmail.com>
2021-06-01 19:41:19 +0000

    pep8 debug and database creation

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,7 +12,7 @@ from model_state import State, Base
 if __name__ == "__main__":
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost/{}"
-        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     newsess = sessionmaker(bind=engine)
     news = newsess()
