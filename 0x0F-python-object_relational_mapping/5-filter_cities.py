3e91ac5df3a7d742c8e7fe1f42700b1d7ccf060b
rkbrian <rkbriankong@gmail.com>
2021-06-01 01:01:46 +0000

    5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Write a script that takes in the name of a state as an argument and
+    lists all cities of that state, using the database hbtn_0e_4_usa
+"""
+
+
+import sys
+from sqlalchemy.sql.sqltypes import BINARY
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost",
+                         sys.argv[1],
+                         sys.argv[2],
+                         sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.name FROM cities \
+    LEFT JOIN states ON cities.state_id = states.id \
+    WHERE BINARY states.name = %s", (sys.argv[4],))
+    states = cursor.fetchall()
+    for i in range(len(states)):
+        print(states[i])
