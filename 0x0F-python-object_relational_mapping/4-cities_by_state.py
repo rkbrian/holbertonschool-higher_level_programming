1f01741238d14c6f4014846b0d458ca33cd2ed16
Ran Kong <rkbriankong@gmail.com>
2021-05-31 19:26:53 -0500

    4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Write a script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost",
+                         sys.argv[1],
+                         sys.argv[2],
+                         sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name FROM
+        cities LEFT JOIN states ON cities.state_id = states.id")
+    states = cursor.fetchall()
+    for i in range(len(states)):
+        print(states[i])
