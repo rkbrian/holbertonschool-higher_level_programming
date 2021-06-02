ea9351bc86f2d714a77f8e67b59cca10946074b3
rkbrian <rkbriankong@gmail.com>
2021-06-02 19:38:45 +0000

    0 - constant print; 1 - print 3 different lines; 2 - process arguments with if and else

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+const varString = 'JavaScript is amazing';
+console.log(varString);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const strOne = 'C is fun';
+const strTwo = 'Python is cool';
+const strThree = 'JavaScript is amazing';
+console.log(strOne);
+console.log(strTwo);
+console.log(strThree);
