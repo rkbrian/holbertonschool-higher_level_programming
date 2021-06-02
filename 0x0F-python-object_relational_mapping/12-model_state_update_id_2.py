f351083b56b8b46310a9abc226301f123efa79af
rkbrian <rkbriankong@gmail.com>
2021-06-02 02:29:01 +0000

    12 debug, 13 delete states with letter a in it

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-    Write a script that changes the name of a State object from the database hbtn_0e_6_usa
+    Write a script that changes the name of a State
+    object from the database hbtn_0e_6_usa
 """
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
