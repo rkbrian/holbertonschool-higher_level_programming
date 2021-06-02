d598acb569b4ab20c9f20fc57aa7a8793355a909
Ran Kong <rkbriankong@gmail.com>
2021-06-01 23:35:45 -0500

    city fetch by state test

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Write a python file that contains the class definition of
+    a City and an instance Base = declarative_base():
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.schema import ForeignKey
+from model_state import Base, State
+
+Base = declarative_base()
+
+
+class City(Base):
+    """inherits from Base Tips and links to the MySQL table cities"""
+
+    __tablename__ = "cities"
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
