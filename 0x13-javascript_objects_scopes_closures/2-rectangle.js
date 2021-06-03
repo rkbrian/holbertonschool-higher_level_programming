9f8bea984b430c7978411b31aa50cb834324e2ed
rkbrian <rkbriankong@gmail.com>
2021-06-03 20:56:31 +0000

    2 - semistandard debug; 3 - rectangle file with instance method print() to print rectangle of text X

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,11 +3,10 @@
 // <class> notation, takes 2 arguments of width and height
 class Rectangle {
   constructor (w, h) {
-    if (w>0 && h>0) {
-    this.width = w;
-    this.height = h;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
 }
 module.exports = Rectangle;
-  
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+// Write a class Rectangle that defines a rectangle:
+// <class> notation, takes 2 arguments of width and height
+// instance method <print()> to print the rectangle using <X>
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+// instance method print()
+Rectangle.prototype.print = function () {
+  for (let i = 0; i < this.height; i++) {
+    const xtext = 'X'.repeat(this.width);
+      console.log(xtext);
+  }
+}
+module.exports = Rectangle;
