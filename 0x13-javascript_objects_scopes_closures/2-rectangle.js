1055d1cab979c17bdc0c9ec5910b9dcff489fa68
Ran Kong <rkbriankong@gmail.com>
2021-06-03 14:26:27 -0500

    1 - debug; 2 - class with 2 var with conditions for main file

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-
+// Write a class Rectangle that defines a rectangle:
+// <class> notation, takes 2 arguments of width and height
+class Rectangle {
+    constructor (w>0, h>0) {
+        this.width = w;
+        this.height = h;
+    }
+  }
+  exports = Rectangle;
+  
