a1c129067d1e8191da2cf829636dcf2b5d85eedb
rkbrian <rkbriankong@gmail.com>
2021-06-03 20:13:13 +0000

    2 debug on if statement

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,12 @@
 // Write a class Rectangle that defines a rectangle:
 // <class> notation, takes 2 arguments of width and height
 class Rectangle {
-    constructor (w>0, h>0) {
-        this.width = w;
-        this.height = h;
+  constructor (w, h) {
+    if (w>0 && h>0) {
+    this.width = w;
+    this.height = h;
     }
   }
-  exports = Rectangle;
+}
+module.exports = Rectangle;
   
