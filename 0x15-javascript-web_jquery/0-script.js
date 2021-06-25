dec70d0f209ac7ca95043344868a56f895e93492
Ran Kong <rkbriankong@gmail.com>
2021-06-25 14:26:44 -0500

    1st push

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// JavaScript script that updates the text color
+//   of the <header> element to red (#FF0000)
+const headercol = document.querySelector('div.header');
+headercol.style.color = "#FF0000";
