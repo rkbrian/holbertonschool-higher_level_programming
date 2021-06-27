643855cb262b7dcda149f067e422bbc9c5fb977a
Ran Kong <rkbriankong@gmail.com>
2021-06-26 23:31:26 -0500

    3 - 4

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// JavaScript script that toggles the class red to the <header>
+// element when the user clicks on the tag DIV#red_header
+const headercol = $("#toggle_header").click(function() {
+    $("header").toggleClass("red green");
+});
