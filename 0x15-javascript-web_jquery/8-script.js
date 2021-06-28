dc6c0206fd1c69afc67f15a4d58d5278d8e56dd4
rkbrian <rkbriankong@gmail.com>
2021-06-28 05:50:33 +0000

    8 unfinished

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 // JavaScript script that fetches and lists the
 //   title for all movies by using a certain URL.
+// UNFINISHED
 const headercol = $("header").click(function() {
     $.get("https://swapi-api.hbtn.io/api/people/5/?format=json",
     function(data) {
-        $("#list_movies").html(data.title);
+        $.each(data.results, )
+	("#list_movies").html(data.title);
     });
 });
