77cbfd845612d2396b5693bcd5416997358215de
rkbrian <rkbriankong@gmail.com>
2021-10-08 22:19:57 +0000

    103 advanced redo for checker testing

---
+++ 0x04-python-more_data_structures/cpython.h
@@ -0,0 +1,17 @@
+#ifndef CPYTHON_H
+#define CPYTHON_H
+
+#include <Python.h>
+#include <listobject.h>
+#include <object.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
+#endif /* CPYTHON_H */
