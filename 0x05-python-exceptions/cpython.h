51ab1e6859fdb6d33d21caf6cf2b52fd058ee2e5
rkbrian <rkbriankong@gmail.com>
2021-11-06 20:20:22 +0000

    cpython: print float object

---
+++ 0x05-python-exceptions/cpython.h
@@ -0,0 +1,22 @@
+#ifndef CPYTHON_H
+#define CPYTHON_H
+
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <longintrepr.h>
+#include <floatobject.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <float.h>
+#include <locale.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+#endif /* CPYTHON_H */
